066e543345c8f341f02eb6a5d444d1054085fb59
Mark Collin <mark.collin@lazeryattack.com>
2014-07-08 06:59:53 +0100

    dynamically define ${TOP_DIR} instead of hard coding it fix errors caused by refactoring other scripts

--- bin/mavenize.sh
+++ bin/mavenize.sh
@@ -27,11 +27,13 @@
 # Where most importantly, you will need to have a working 1.4 compiler executable specified as the value for javac14-compiler-executable 
 #
 
+#TODO offer option to set this via command line or env variable
 if [ "" == "$GPGPASSPHRASE" ]; then
 	echo "Environment variable GPGPASSPHRASE must be defined"
 	exit 1
 fi
 
+#TODO read these in from command line
 export RSYNTAXTEXTAREA_VERSION=2.5.1
 export AUTOCOMPLETE_VERSION=2.5.1
 export SPELLCHECKER_VERSION=2.5.1
@@ -62,17 +64,21 @@ export RTEXTCOMMON_VERSION=2.0.7
 # RSYNTAXTEXTAREA_VERSION
 #
 #
+#TODO read this in via command line
 export GITCOMMITID=738d8cba6a9120313394e3531fd686f8600abd06
 export LANGUAGESUPPORT_VERSION=$RSYNTAXTEXTAREA_VERSION
 
 
-TOP_DIR=/home/manningr/workspace/rtext4maven/trunk
-BIN_DIR=$TOP_DIR/bin
-OFFICIAL_SOURCE_DIR=$TOP_DIR/official-jars/source
-OFFICIAL_COMPILED_DIR=$TOP_DIR/official-jars/compiled
+CURRENT_DIRECTORY=`pwd`
+SCRIPT_DIRECTORY=`dirname $0`
+TOP_DIR=${CURRENT_DIRECTORY}${SCRIPT_DIRECTORY#?}
+TOP_DIR=${TOP_DIR%/*}
+BIN_DIR=${TOP_DIR}/bin
+OFFICIAL_SOURCE_DIR=${TOP_DIR}/official-jars/source
+OFFICIAL_COMPILED_DIR=${TOP_DIR}/official-jars/compiled
 
 # This is the root directory of the mavenized RText artifacts.
-export OUTPUT_DIR=$TOP_DIR/output
+export OUTPUT_DIR=${TOP_DIR}/output
 
 export SF_PROJECT_URI_BASE=http://sourceforge.net/projects
 
@@ -134,11 +140,11 @@ function mavenizemodule() {
     projDir=$1
     sourceDirOrJar=$2
     projVersion=$3
-    
+
     echo "Mavenizing module: projDir=${projDir} sourceDirOrJar=${sourceDirOrJar} projVersion=${projVersion}"
 
     cd $projDir
-    /bin/bash $BIN_DIR/mavenize-module.sh $sourceDirOrJar $projVersion  
+    /bin/bash $BIN_DIR/mavenize-module.sh -f=$sourceDirOrJar -v=$projVersion
     if [ "$?" -ne "0" ]; then
         echo "Failed to mavenize module: $projDir"
         exit 1
@@ -220,18 +226,18 @@ mkdir -p $ICONGROUPS_PROJ_DIR
 mkdir -p $RTEXT_PROJ_DIR
 
 # copy in the pom and pom template files.
-cp poms/root-pom.xml			  $OUTPUT_DIR/pom.xml
-cp poms/rsyntaxtearea-module-pom.xml      $RSYNTAXTEXTAREA_MODULES_DIR/pom.xml
-cp poms/autocomplete-pom.xml.template     $AUTOCOMPLETE_PROJ_DIR/pom.xml
-cp poms/languagesupport-pom.xml.template  $LANGUAGESUPPORT_PROJ_DIR/pom.xml
-cp poms/rsyntaxtextarea-pom.xml  	  $RSYNTAXTEXTAREA_PROJ_DIR/pom.xml
-cp poms/spellchecker-pom.xml.template     $SPELLCHECKER_PROJ_DIR/pom.xml
-cp poms/rstaui-pom.xml                    $RSTA_UI_PROJ_DIR/pom.xml
+cp ${TOP_DIR}/poms/root-pom.xml                     $OUTPUT_DIR/pom.xml
+cp ${TOP_DIR}/poms/rsyntaxtearea-module-pom.xml     $RSYNTAXTEXTAREA_MODULES_DIR/pom.xml
+cp ${TOP_DIR}/poms/autocomplete-pom.xml.template    $AUTOCOMPLETE_PROJ_DIR/pom.xml
+cp ${TOP_DIR}/poms/languagesupport-pom.xml.template $LANGUAGESUPPORT_PROJ_DIR/pom.xml
+cp ${TOP_DIR}/poms/rsyntaxtextarea-pom.xml  	    $RSYNTAXTEXTAREA_PROJ_DIR/pom.xml
+cp ${TOP_DIR}/poms/spellchecker-pom.xml.template    $SPELLCHECKER_PROJ_DIR/pom.xml
+cp ${TOP_DIR}/poms/rstaui-pom.xml                   $RSTA_UI_PROJ_DIR/pom.xml
 
-cp poms/rtext-module-pom.xml		  $RTEXT_MODULES_DIR/pom.xml
-cp poms/common-pom.xml.template		  $COMMON_PROJ_DIR/pom.xml
-cp poms/icongroups-pom.xml		  $ICONGROUPS_PROJ_DIR/pom.xml
-cp poms/rtext-pom.xml.template		  $RTEXT_PROJ_DIR/pom.xml
+cp ${TOP_DIR}/poms/rtext-module-pom.xml		        $RTEXT_MODULES_DIR/pom.xml
+cp ${TOP_DIR}/poms/common-pom.xml.template		    $COMMON_PROJ_DIR/pom.xml
+cp ${TOP_DIR}/poms/icongroups-pom.xml		        $ICONGROUPS_PROJ_DIR/pom.xml
+cp ${TOP_DIR}/poms/rtext-pom.xml.template		    $RTEXT_PROJ_DIR/pom.xml
 
 # pull down the official source and compiled zip files from sourceforge if necessary
 downloadArchives rsyntaxtextarea $RSYNTAXTEXTAREA_VERSION $RSYNTAX_BASE $RSYNTAXTEXTAREA_SOURCE_ARCHIVE $RSYNTAXTEXTAREA_COMPILE_ARCHIVE
@@ -245,27 +251,20 @@ downloadArchives rtext $RTEXT_VERSION $RTEXT_BASE $RTEXT_SOURCE_ARCHIVE $RTEXT_C
 # languagesupport module isn't released, so we need to clone it from git.
 if [ ! -d "$OFFICIAL_SOURCE_DIR/language-support/$LANGUAGESUPPORT_VERSION" ]; then
     mkdir -p "$OFFICIAL_SOURCE_DIR/language-support/$LANGUAGESUPPORT_VERSION"
-    /bin/bash $BIN_DIR/exportRevision.sh  $GITCOMMITID "$OFFICIAL_SOURCE_DIR/language-support/$LANGUAGESUPPORT_VERSION"
+    /bin/bash $BIN_DIR/exportRevision.sh  -r=$GITCOMMITID -e="$OFFICIAL_SOURCE_DIR/language-support/$LANGUAGESUPPORT_VERSION"
     if [ "$?" != 0 ]; then
 	exit 1
     fi
 fi
 
 # run mavenize-module.sh on each module, giving it the location of the source archive
-mavenizemodule -f=$RSYNTAXTEXTAREA_PROJ_DIR -v="$OFFICIAL_SOURCE_DIR/$RSYNTAXTEXTAREA_VERSION/$RSYNTAXTEXTAREA_SOURCE_ARCHIVE" -c=$RSYNTAXTEXTAREA_VERSION
-
-mavenizemodule -f=$AUTOCOMPLETE_PROJ_DIR -v="$OFFICIAL_SOURCE_DIR/$AUTOCOMPLETE_VERSION/$AUTOCOMPLETE_SOURCE_ARCHIVE" -c=$AUTOCOMPLETE_VERSION
-
-mavenizemodule -f=$SPELLCHECKER_PROJ_DIR -v="$OFFICIAL_SOURCE_DIR/$SPELLCHECKER_VERSION/$SPELLCHECKER_SOURCE_ARCHIVE" -c=$SPELLCHECKER_VERSION
-
-mavenizemodule -f=$RSTA_UI_PROJ_DIR -v="$OFFICIAL_SOURCE_DIR/$RSTA_UI_VERSION/$RSTA_UI_SOURCE_ARCHIVE" -c=$RSTA_UI_VERSION
-
-mavenizemodule -f=$LANGUAGESUPPORT_PROJ_DIR -v="$OFFICIAL_SOURCE_DIR/language-support/$LANGUAGESUPPORT_VERSION" -c="r${LANGUAGESUPPORT_VERSION}"
-
-mavenizemodule -f=$COMMON_PROJ_DIR -v="$OFFICIAL_SOURCE_DIR/$RTEXTCOMMON_VERSION/$COMMON_SOURCE_ARCHIVE" -c=$RTEXTCOMMON_VERSION
-
-echo "Mavenizing RTEXT module with :  mavenizemodule" -c=$RTEXT_PROJ_DIR $OFFICIAL_SOURCE_DIR/$RTEXT_VERSION/$RTEXT_SOURCE_ARCHIVE $RTEXT_VERSION
-mavenizemodule -f=$RTEXT_PROJ_DIR -v="$OFFICIAL_SOURCE_DIR/$RTEXT_VERSION/$RTEXT_SOURCE_ARCHIVE" -c=$RTEXT_VERSION
+mavenizemodule $RSYNTAXTEXTAREA_PROJ_DIR "$OFFICIAL_SOURCE_DIR/$RSYNTAXTEXTAREA_VERSION/$RSYNTAXTEXTAREA_SOURCE_ARCHIVE" $RSYNTAXTEXTAREA_VERSION
+mavenizemodule $AUTOCOMPLETE_PROJ_DIR "$OFFICIAL_SOURCE_DIR/$AUTOCOMPLETE_VERSION/$AUTOCOMPLETE_SOURCE_ARCHIVE" $AUTOCOMPLETE_VERSION
+mavenizemodule $SPELLCHECKER_PROJ_DIR "$OFFICIAL_SOURCE_DIR/$SPELLCHECKER_VERSION/$SPELLCHECKER_SOURCE_ARCHIVE" $SPELLCHECKER_VERSION
+mavenizemodule $RSTA_UI_PROJ_DIR "$OFFICIAL_SOURCE_DIR/$RSTA_UI_VERSION/$RSTA_UI_SOURCE_ARCHIVE" $RSTA_UI_VERSION
+mavenizemodule $LANGUAGESUPPORT_PROJ_DIR "$OFFICIAL_SOURCE_DIR/language-support/$LANGUAGESUPPORT_VERSION" "r${LANGUAGESUPPORT_VERSION}"
+mavenizemodule $COMMON_PROJ_DIR "$OFFICIAL_SOURCE_DIR/$RTEXTCOMMON_VERSION/$COMMON_SOURCE_ARCHIVE" $RTEXTCOMMON_VERSION
+mavenizemodule $RTEXT_PROJ_DIR "$OFFICIAL_SOURCE_DIR/$RTEXT_VERSION/$RTEXT_SOURCE_ARCHIVE" $RTEXT_VERSION
 
 
 # build each module in dependency order and compare the maven-built jar to the official jar.
@@ -285,16 +284,18 @@ mvn clean source:jar javadoc:jar install
 /bin/bash $BIN_DIR/prepareBundle.sh $ICONGROUPS_PROJ_DIR/target
 
 
-# Since LANGUAGE_SUPPORT modules is not officially released, we cannot compare it to anything.
-# TODO: Not exactly - it can be found in the RText artifact: rtext/plugins/language_support.jar
-compj $AUTOCOMPLETE_PROJ_DIR/target/autocomplete-${AUTOCOMPLETE_VERSION}.jar $OFFICIAL_COMPILED_DIR/$AUTOCOMPLETE_VERSION/autocomplete.jar
-compj $RSYNTAXTEXTAREA_PROJ_DIR/target/rsyntaxtextarea-${RSYNTAXTEXTAREA_VERSION}.jar  $OFFICIAL_COMPILED_DIR/$RSYNTAXTEXTAREA_VERSION/rsyntaxtextarea.jar
-compj $SPELLCHECKER_PROJ_DIR/target/spellchecker-${SPELLCHECKER_VERSION}.jar $OFFICIAL_COMPILED_DIR/$SPELLCHECKER_VERSION/rsta_spellchecker.jar
-compj $RSTA_UI_PROJ_DIR/target/rstaui-${RSTA_UI_VERSION}.jar $OFFICIAL_COMPILED_DIR/$RSTA_UI_VERSION/rstaui.jar
-compj $RTEXT_PROJ_DIR/target/rtext-${RTEXT_VERSION}.jar $OFFICIAL_COMPILED_DIR/$RTEXT_VERSION/rtext/RText.jar
-compj $COMMON_PROJ_DIR/target/common-${RTEXTCOMMON_VERSION}.jar $OFFICIAL_COMPILED_DIR/$RTEXTCOMMON_VERSION/rtext/fife.common.jar
-compj $LANGUAGESUPPORT_PROJ_DIR/target/languagesupport-r${LANGUAGESUPPORT_VERSION}.jar $OFFICIAL_COMPILED_DIR/$RTEXTCOMMON_VERSION/rtext/plugins/language_support.jar
-#compj $ICONGROUPS_PROJ_DIR/target/
+#TODO just tray a SHA1 comparison?
+#
+## Since LANGUAGE_SUPPORT modules is not officially released, we cannot compare it to anything.
+## TODO: Not exactly - it can be found in the RText artifact: rtext/plugins/language_support.jar
+#compj $AUTOCOMPLETE_PROJ_DIR/target/autocomplete-${AUTOCOMPLETE_VERSION}.jar $OFFICIAL_COMPILED_DIR/$AUTOCOMPLETE_VERSION/autocomplete.jar
+#compj $RSYNTAXTEXTAREA_PROJ_DIR/target/rsyntaxtextarea-${RSYNTAXTEXTAREA_VERSION}.jar  $OFFICIAL_COMPILED_DIR/$RSYNTAXTEXTAREA_VERSION/rsyntaxtextarea.jar
+#compj $SPELLCHECKER_PROJ_DIR/target/spellchecker-${SPELLCHECKER_VERSION}.jar $OFFICIAL_COMPILED_DIR/$SPELLCHECKER_VERSION/rsta_spellchecker.jar
+#compj $RSTA_UI_PROJ_DIR/target/rstaui-${RSTA_UI_VERSION}.jar $OFFICIAL_COMPILED_DIR/$RSTA_UI_VERSION/rstaui.jar
+#compj $RTEXT_PROJ_DIR/target/rtext-${RTEXT_VERSION}.jar $OFFICIAL_COMPILED_DIR/$RTEXT_VERSION/rtext/RText.jar
+#compj $COMMON_PROJ_DIR/target/common-${RTEXTCOMMON_VERSION}.jar $OFFICIAL_COMPILED_DIR/$RTEXTCOMMON_VERSION/rtext/fife.common.jar
+#compj $LANGUAGESUPPORT_PROJ_DIR/target/languagesupport-r${LANGUAGESUPPORT_VERSION}.jar $OFFICIAL_COMPILED_DIR/$RTEXTCOMMON_VERSION/rtext/plugins/language_support.jar
+##compj $ICONGROUPS_PROJ_DIR/target/
 
 echo "To use meld, execute the following commands (The official released jar is the one without a version): "
 
@@ -305,7 +306,5 @@ echo "meld /tmp/rstaui-${RSTA_UI_VERSION}.jar.extracted /tmp/rstaui.jar.extracte
 echo "meld /tmp/rtext-${RTEXT_VERSION}.jar.extracted /tmp/RText.jar.extracted"
 echo "meld /tmp/common-${RTEXTCOMMON_VERSION}.jar.extracted /tmp/fife.common.jar.extracted"
 echo "meld /tmp/languagesupport-r${LANGUAGESUPPORT_VERSION}.jar.extracted /tmp/language_support.jar.extracted"
-
-
 
 exit 0
